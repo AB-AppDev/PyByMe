771d310f8698f548348eecbb62206c059f2bd9e4
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 14:12:26 +0530

    SetsAndDectionary Code

---
+++ Code For Beginners/SetDiction.py
@@ -0,0 +1,23 @@
+""" * Creating A Set"""
+
+set = {1, 2, 3, 4, 5}
+print(set)
+print()
+
+"""
+No indexing Allowded
+
+>print(s[0])
+
+Line Generating err
+"""
+
+""" * Creating A Dictionary"""
+dict = {"Name": "Abhishek", "RollNum": 2}
+print(dict["Name"])
+print(dict["RollNum"])
+print()
+
+"""
+dictionary element has to pass key value in brackets
+"""
