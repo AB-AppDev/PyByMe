2b6a46e384b573d90abfcff505c0bab1a1b180ea
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 15:02:25 +0530

    Ary_Operations

---
+++ Code For Beginners/Ary_Op.py
@@ -0,0 +1,17 @@
+"""Unary"""
+N = 10
+print(-N)
+
+'''Relational'''
+
+print("Check 10 > 50 = " + str(10 > 50))
+print("Check 10 > 5 = " + str(10 > 5))
+print("Check 10 = 5 = " + str(10 == 5))
+print("Check 10 >= 10 = " + str(10 >= 10))
+print("Check 10 != 10 = " + str(10 != 10))
+
+'''Logical'''
+
+print("ANDing - " + str(True and False))
+print("ORing - " + str(True or False))
+print("Not True - " + str(not True))
