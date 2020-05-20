2747496ab7c4894aaccecab9cb5247ea9f7e0cc6
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 14:41:29 +0530

    Arithmatic and Assignment

---
+++ Code For Beginners/AithAssign_Op.py
@@ -0,0 +1,30 @@
+X = 14
+Y = 4
+
+ad = X + Y
+
+print(ad)
+print(X - Y)
+print(X * Y)
+print(X / Y)
+
+'''Int by Floor Division Method'''
+print(X // Y)
+print(X % Y)
+
+'''Power'''
+
+print(X ** 3)
+
+'''Swap'''
+
+print()
+print(X)
+print(Y)
+
+Z = X
+X = Y
+Y = Z
+print()
+print(X)
+print(Y)
