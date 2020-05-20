160230ae9ec5b164ba850e255bc74975102926ef
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 13:38:11 +0530

    Update Sequence.py

--- Code For Beginners/Sequence.py
+++ Code For Beginners/Sequence.py
@@ -3,7 +3,13 @@
 set = {1, 2, 3, 4, 5}
 print(set)
 
-
-""""No indexing Allowded
+"""
+No indexing Allowded
 print(s[0])
-Line Generating err"""
+Line Generating err
+"""
+
+""" * Creating A List"""
+
+myList = [2, "Abhishek Bhalerao", 8485842407]
+print(myList)
