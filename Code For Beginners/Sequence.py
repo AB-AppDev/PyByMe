f3d2f4c7308b37f463328db3b5350420d1d44886
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 14:07:09 +0530

    Update Sequence.py

--- Code For Beginners/Sequence.py
+++ Code For Beginners/Sequence.py
@@ -12,17 +12,45 @@ No indexing Allowded
 Line Generating err
 """
 
-""" * Creating A List"""
+""" * Creating A List - []"""
 
 myList = [2, "Abhishek Bhalerao", "84xxx424xx"]
 print(myList)
 print(myList[2])
 print()
 
-""" * Creating A tuple"""
+""" * Creating A tuple - ()"""
 
 myTuple = (2, "Abhishek Bhalerao", "84xxx424xx")
 print(myTuple[0])
+print()
+"""
+No outside indexing Allowded
+
+>print(myTuple[3])
+
+Line Generating err - 
+    print(myTuple[3])
+IndexError: tuple index out of range
+"""
+
+""" * Creating A Range"""
+
+'''Print 1to9'''
+rng = range(1, 10)
+q = list(rng)
+print(q)
+
+'''OddNums'''
+rng = range(1, 10, 2)
+q = list(rng)
+print(q)
+
+'''EvenNums'''
+rng = range(2, 10, 2)
+q = list(rng)
+print(q)
+
 """
 No outside indexing Allowded
 
