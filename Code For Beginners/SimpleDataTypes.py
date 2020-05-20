3926b13bdc7bf7f903971e4b82cb39bf1e42cc95
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 13:25:53 +0530

    PyCEd

--- Code For Beginners/SimpleDataTypes.py
+++ Code For Beginners/SimpleDataTypes.py
@@ -1,12 +1,13 @@
-Name = "Abhi" 
+Name = "Abhi"
 RollNo = 2
-print("Data\nName - "+Name+"\nRoll Number - "+str(RollNo))
+
+print("Data\nName - " + Name + "\nRoll Number - " + str(RollNo))
 
 NoData = None
-print("NoData - "+ str(NoData))
+print("NoData - " + str(NoData))
 
 isGreater = False
 print(isGreater)
 
 isGreater = 15 > 5
-print("Is 15 is greater than 5 "+str(isGreater))
+print("Is 15 is greater than 5 " + str(isGreater))
