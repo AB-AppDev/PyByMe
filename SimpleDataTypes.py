f114e735e3c200cd383274dad60c3528f670f013
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-20 12:31:14 +0530

    Update SimpleDataTypes.py

--- SimpleDataTypes.py
+++ SimpleDataTypes.py
@@ -3,4 +3,10 @@ RollNo = 2
 print("Data\nName - "+Name+"\nRoll Number - "+str(RollNo))
 
 NoData = None
-print("NoData - "+ str(NoData ))
+print("NoData - "+ str(NoData))
+
+isGreater = False
+print(isGreater)
+
+isGreater = 15 > 5
+print("Is 15 is greater than 5 "+str(isGreater))
