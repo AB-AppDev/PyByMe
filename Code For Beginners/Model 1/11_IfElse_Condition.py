3c9d6db38b5d8eb973c3157a6815fa34b3f8109b
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-22 13:53:27 +0530

    Create 11_IfElse_Condition.py

---
+++ Code For Beginners/Model 1/11_IfElse_Condition.py
@@ -0,0 +1,22 @@
+age = 19
+
+if age >= 18:
+    print("You are eligible for voting")
+
+"""--------------"""
+x = -10
+
+if x >= 0:
+    print("Positive")
+else:
+    print("Negative")
+
+"""--------------"""
+
+light = "red"
+if light == "red":
+    print("STOP")
+elif light == "yellow":
+    print("SEE")
+else:
+    print("GO")
