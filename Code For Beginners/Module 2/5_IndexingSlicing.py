1e0dac92c93b705d5358f72eb6c95b0f94f018cc
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 11:36:34 +0530

    Create 5_IndexingSlicing.py

---
+++ Code For Beginners/Module 2/5_IndexingSlicing.py
@@ -0,0 +1,15 @@
+# Slicing
+
+list1 = [100, 101, 102, 103, 104, 105]
+tuple1 = (00, 10, 20, 30, 40, 50)
+string1 = "ABHISHEK"
+
+print(list1[0:5:2])
+print(list1[0:3])
+print(list1[3:])
+print(list1[:2])
+print(list1[::2])
+
+#Negative Indexing
+print(string1[::-1])
+print(string1[:3:-1])
