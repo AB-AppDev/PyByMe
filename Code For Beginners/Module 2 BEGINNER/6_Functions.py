770474d978ab6faa54cb1a2e51887d146e7ae652
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 12:16:59 +0530

    Python

--- Code For Beginners/Module 2 BEGINNER/6_Functions.py
+++ Code For Beginners/Module 2 BEGINNER/6_Functions.py
@@ -1,4 +1,6 @@
 def maxfromtwo(a, b):
+    global c
+    c = b
     if a > b:
         return a
     else:
@@ -16,3 +18,5 @@ def info2(name, roll, **marks):
 print(maxfromtwo(5, 10))
 info("\nAbhi", 2, 70, 60, 65)
 info2(name="\nAbhi", roll=2, eng=70, mar=60, hin=65)
+
+print(c)
