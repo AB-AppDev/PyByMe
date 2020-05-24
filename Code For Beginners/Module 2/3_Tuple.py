ca7fee2f5154196fd0b1bf2115dd490a14ab3881
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 10:41:13 +0530

    Create 3_Tuple.py

---
+++ Code For Beginners/Module 2/3_Tuple.py
@@ -0,0 +1,12 @@
+tuple_1 = (10, 20, 30, 40, 50)
+print(tuple_1)
+
+tuple_2 = (60, 70, 80, 90, 100)
+print(tuple_2)
+
+tuple_3 = tuple_1 + tuple_2
+print(tuple_3)
+
+print(tuple_3[5])
+print(max(tuple_3))
+
