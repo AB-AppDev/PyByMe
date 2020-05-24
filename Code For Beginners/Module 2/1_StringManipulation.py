2ee44e33be6dd0771bab8c6cedf9fbce65f4c556
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 10:27:29 +0530

    Update 1_StringManipulation.py

--- Code For Beginners/Module 2/1_StringManipulation.py
+++ Code For Beginners/Module 2/1_StringManipulation.py
@@ -4,15 +4,18 @@ y = "WORLD"
 print(x)
 print(y)
 
-print(x,y)
+print(x, y)
 
 print(x.capitalize())
-
 
 print(x.upper())
 print(y.lower())
 
-
 print(x[1])
 
 print(x[3:])
+
+if x.islower():
+    z = x.upper()+y
+
+print(z.split(" "))
