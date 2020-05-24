98e9053e6189baeee930e882add5b7da183806eb
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 12:08:38 +0530

    Create 6_Functions.py

---
+++ Code For Beginners/Module 2/6_Functions.py
@@ -0,0 +1,18 @@
+def maxfromtwo(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def info(name, roll, *marks):
+    print(name, roll, marks)
+
+
+def info2(name, roll, **marks):
+    print(name, roll, marks)
+
+
+print(maxfromtwo(5, 10))
+info("\nAbhi", 2, 70, 60, 65)
+info2(name="\nAbhi", roll=2, eng=70, mar=60, hin=65)
