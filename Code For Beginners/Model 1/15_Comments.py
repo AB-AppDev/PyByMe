e4bf67fc53d36a889cdde2f4f4e09f35fb004026
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 10:08:29 +0530

    Create 15_Comments.py

---
+++ Code For Beginners/Model 1/15_Comments.py
@@ -0,0 +1,9 @@
+# Adding hash charecter makes single commented
+"""
+Stings in three qoutation marks,
+makes multiple line commented
+
+This will not executed by interpreter.
+"""
+
+print("\nNote:-\n# Adding hash charecter makes single commented \n\"\"\" Stings in three qoutation marks, \nmakes multiple line commented \n\nThis will not executed by interpreter. \"\"\" ")
