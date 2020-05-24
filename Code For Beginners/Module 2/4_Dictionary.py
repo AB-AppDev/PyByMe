741013d06387a63b7a783f15b9fd845d096ad124
AB-AppDev <ab.appdevworks@gmail.com>
2020-05-24 10:52:50 +0530

    Create 4_Dictionary.py

---
+++ Code For Beginners/Module 2/4_Dictionary.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "name": "abhishek",
+    "age": 20,
+    "college": "metioe",
+    "class": "co_se_b"
+}
+
+print(my_dict)
+
+print(my_dict["age"])
+print(my_dict.pop("age"))
+print(my_dict)
