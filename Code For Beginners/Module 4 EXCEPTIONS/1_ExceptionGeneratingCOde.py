845b245c60a0c8834d4194251cbea5b76ea4d30b
AB-AppDev <ab.appdevworks@gmail.com>
2020-06-01 16:51:42 +0530

    OOPS

---
+++ Code For Beginners/Module 4 EXCEPTIONS/1_ExceptionGeneratingCOde.py
@@ -0,0 +1,13 @@
+Nr = 215
+Dr = 0
+
+print(Nr / Dr)
+
+'''
+
+Traceback (most recent call last):
+  File "1_ExceptionGeneratingCOde.py", line 4, in <module>
+    print(Nr / Dr)
+ZeroDivisionError: division by zero
+
+'''
