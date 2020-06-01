2c5dee160277a821a7f82ec4a586a5f85575bf09
AB-AppDev <ab.appdevworks@gmail.com>
2020-06-01 16:42:54 +0530

    OOPS

--- Code For Beginners/Module 3 OOPS/11_OperOverload.py
+++ Code For Beginners/Module 3 OOPS/11_OperOverload.py
@@ -18,9 +18,12 @@ class Books:
     def __add__(self, other):
         return self.pages + other.pages
 
+    def __gt__(self, other):
+        return self.pages > other.pages;
 
-b1 = Books(100)
-b2 = Books(150)
 
-print(b1 + b2)
-#print(b1 + b2)
+javabook = Books(100)
+pythonbook = Books(150)
+
+print(javabook.__add__(pythonbook))
+print(pythonbook.__gt__(javabook))
