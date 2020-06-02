31e270922d3e84289a5f5f873bdaa5bb88e20349
AB-AppDev <ab.appdevworks@gmail.com>
2020-06-02 13:14:42 +0530

    Excercise

---
+++ PyProjects/PasswordGenerate/main.py
@@ -0,0 +1,20 @@
+import random
+
+upChar = ["A", "B", "C", "E", "D"]
+loChar = ["a", "b", "c", "d", "e"]
+numChar = [1, 2, 3, 4, 5]
+spclChar = ["!", "@", "#", "$", "%"]
+
+wordlist = []
+with open("C:\\Users\\abhis\\Desktop\\PyByMe\\simText.txt", "r") as file:
+    data = file.readlines()
+
+    for line in data:
+        words = line.split()
+        for item in words:
+            if len(item) > 5:
+                wordlist.append(item.capitalize())
+
+paswd = random.choice(upChar) + random.choice(loChar) + random.choice(wordlist) + random.choice(loChar) + str(
+    random.choice(numChar)) + random.choice(upChar) + random.choice(spclChar) + random.choice(loChar)
+print(paswd)
---
+++ PyProjects/PasswordGenerate/random_pass.py
@@ -0,0 +1,10 @@
+import random
+
+names = ["Abhi", "Yogesh", "Devesh", "Kunal"]
+
+print(random.choice(names))
+
+print(random.randint(000000, 999999))
+
+print(random.random())
+
